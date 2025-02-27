bf904e536b6dd49b17cc355e34c475e0e57e4d7d
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-02-27 11:37:18 +0100

    add deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,60 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const DotenvWebpackPlugin = require('dotenv-webpack');
-const webpack = require('webpack');
+const DotEnv = require('dotenv-webpack');
+const webpack = require('webpack'); // Added this line
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new DotEnv(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
 
 module.exports = ({ mode }) => {
     const isProductionMode = mode === 'prod';
     const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
-
     const API_URL = isProductionMode
         ? process.env.API_URL_PROD 
         : process.env.API_URL; 
+        
+    console.log(`Using API URL: ${API_URL}`); // Log the API URL being used for verification
 
-    const baseConfig = {
-        entry: path.resolve(__dirname, './src/index'),
-        mode: 'development',
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: ['style-loader', 'css-loader'],
-                },
-                { test: /\.ts$/i, use: 'ts-loader' },
-            ],
-        },
-        resolve: {
-            extensions: ['.ts', '.js'],
-        },
-        output: {
-            filename: 'index.js',
-            path: path.resolve(__dirname, './dist'),
-        },
+    return merge(baseConfig, envConfig, {
         plugins: [
-            new DotenvWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, './src/index.html'),
-                filename: 'index.html',
-            }),
-            new CleanWebpackPlugin(),
-
             new webpack.DefinePlugin({
                 'process.env.API_URL': JSON.stringify(API_URL),
-                'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
             }),
         ],
-    };
-
-    return merge(baseConfig, envConfig);
+    });
 };
