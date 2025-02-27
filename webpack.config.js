bbe0b6db227c6601fab867ae0dc5bdcd2d81e573
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-02-27 11:51:00 +0100

    add deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
-const webpack = require('webpack'); // Added this line
-
+const webpack = require('webpack'); 
+console.log(`Environment Variables: API_URL=${process.env.API_URL}, API_URL_PROD=${process.env.API_URL_PROD}, API_KEY=${process.env.API_KEY}`); 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
@@ -45,17 +45,14 @@ const baseConfig = {
 module.exports = ({ mode }) => {
     const isProductionMode = mode === 'prod';
     const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+    
+    
+
     const API_URL = isProductionMode
         ? process.env.API_URL_PROD 
         : process.env.API_URL; 
         
-    console.log(`Using API URL: ${API_URL}`); // Log the API URL being used for verification
+    console.log(`Using API URL: ${API_URL}`);
 
-    return merge(baseConfig, envConfig, {
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env.API_URL': JSON.stringify(API_URL),
-            }),
-        ],
-    });
+    return merge(baseConfig, envConfig);
 };
