b9941c3151e00ca88acd2b52a144405d13e9af2d
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-02-27 14:25:20 +0100

    add deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,41 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
-const webpack = require('webpack'); 
-console.log(`Environment Variables: API_URL=${process.env.API_URL}, API_URL_PROD=${process.env.API_URL_PROD}, API_KEY=${process.env.API_KEY}`); 
-const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.ts'),
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    plugins: [
-        new DotEnv(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
-    ],
-};
+const webpack = require('webpack');
 
 module.exports = ({ mode }) => {
     const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
-    
-    
 
-    const API_URL = isProductionMode
-        ? process.env.API_URL_PROD 
-        : process.env.API_URL; 
-        
-    console.log(`Using API URL: ${API_URL}`);
+    const envFile = isProductionMode ? '.env.production' : '.env.development';
 
-    return merge(baseConfig, envConfig);
+    console.log(`Webpack mode: ${mode}`);
+    console.log(`Using env file: ${envFile}`);
+
+    const baseConfig = {
+        entry: path.resolve(__dirname, './src/index.ts'),
+        mode: isProductionMode ? 'production' : 'development',
+        module: {
+            rules: [
+                { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+                { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+                { test: /\.svg$/, loader: 'svg-inline-loader' },
+            ],
+        },
+        resolve: { extensions: ['.ts', '.js'] },
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, './dist'),
+        },
+        plugins: [
+            new DotEnv({ path: path.resolve(__dirname, envFile) }),
+
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, './src/index.html'),
+                filename: 'index.html',
+            }),
+            new CleanWebpackPlugin(),
+        ],
+    };
+
+    return merge(baseConfig, isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config'));
 };
